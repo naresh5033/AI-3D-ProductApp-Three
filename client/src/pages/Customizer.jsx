ddce925ec5478ce62ca0ca9ac3c4c89eab3480af
Naresh kumar <nareshxo@Nareshs-Air.wind3.hub>
2023-07-22 14:39:40 +0200

    both frontend and backend is deployed and the app is live

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -60,7 +60,7 @@ const Customizer = () => {
     try {
       setGeneratingImg(true); //we wana start loading
 
-      const response = await fetch("http://localhost:8080/api/v1/dalle", {
+      const response = await fetch("https://ai-3d-product-backend.onrender.com/api/v1/dalle", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
